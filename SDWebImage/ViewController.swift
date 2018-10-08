1125e1598458bf319511e97fd323a1b04ece75e1
sunset12321215 <sunsetno08@gmail.com>
2018-10-08 20:39:07 +0700

    commit to branch master

--- SDWebImage/ViewController.swift
+++ SDWebImage/ViewController.swift
@@ -8,17 +8,18 @@
 
 import UIKit
 
+//import sdwe
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("SD Web Image")
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
